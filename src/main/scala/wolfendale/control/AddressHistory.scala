3ee14bb242c7f8725da592211e2f08bf7c724a2d
Michael Wolfendale <4563722+wolfendale@users.noreply.github.com>
2021-09-28 08:49:34 +0100

    Fix compilation error for examples

--- src/main/scala/wolfendale/control/AddressHistory.scala
+++ src/main/scala/wolfendale/control/AddressHistory.scala
@@ -1,6 +1,7 @@
 package wolfendale.control
 
 import cats.effect.IO
+import cats.effect.unsafe.implicits.global
 import cats.implicits._
 import wolfendale.control.syntax._
 
--- src/main/scala/wolfendale/control/V2App.scala
+++ src/main/scala/wolfendale/control/V2App.scala
@@ -1,6 +1,7 @@
 package wolfendale.control
 
 import cats.effect._
+import cats.effect.unsafe.implicits.global
 import wolfendale.control.syntax._
 
 object V2App extends App {
@@ -12,10 +13,8 @@ object V2App extends App {
     c <- IO.pure(3).annotated("baz")
   } yield a + b + c
 
-
   val (current, (history, result)) = program.runWith(Machine.runUntil("boom")).unsafeRunSync()
   println("current", current)
   println("result", result)
   println("history", history)
-
 }
